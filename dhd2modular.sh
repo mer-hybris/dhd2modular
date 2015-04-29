c1f4d8420506aedfc75a51c33e91b239eb276a0d
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2015-04-29 21:59:19 +0100

    no need for qtsensors

--- dhd2modular.sh
+++ dhd2modular.sh
@@ -322,7 +322,6 @@ elif [[ 'build-modular' == $1* ]]; then
     buildmw qt5-feedback-haptics-droid-vibrator || die
     buildmw qt5-qpa-hwcomposer-plugin || die
     buildmw "https://github.com/mer-hybris/qtscenegraph-adaptation.git" rpm/qtscenegraph-adaptation-droid.spec || die
-    buildmw "https://github.com/mer-packages/qtsensors.git" || die
     buildmw "https://github.com/mer-packages/sensorfw.git" rpm/sensorfw-qt5-hybris.spec || die
     read -p '"Build HA Middleware Packages built". Press Enter to continue.'
     popd
